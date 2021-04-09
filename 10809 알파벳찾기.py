9f119a4314e8ba943979a868a31eaee8d56187b2
jlee99 <58466461+ammumal@users.noreply.github.com>
2021-04-10 00:17:05 +0900

    Create 10809 알파벳찾기.py

---
+++ 10809 알파벳찾기.py
@@ -0,0 +1,5 @@
+S = input()
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in range(len(alpha)):
+    print(S.find(alpha[i]), end =' ')
