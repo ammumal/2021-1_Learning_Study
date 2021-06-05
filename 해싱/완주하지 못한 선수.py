7b47a15e5e29a797be69c13b33828ad9ef22456d
jlee99 <58466461+ammumal@users.noreply.github.com>
2021-06-05 21:33:44 +0900

    Create 완주하지 못한 선수.py

---
+++ 해싱/완주하지 못한 선수.py
@@ -0,0 +1,6 @@
+import collections
+
+def solution(participant, completion):
+    result = collections.Counter(participant) - collections.Counter(completion)
+    answer = list(result.keys())
+    return answer[0]
